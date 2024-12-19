85eb54e69b7da8d456fd7137dfd743f26165a89c
avindhawale <avin.dhawale@gmail.com>
2024-12-19 08:45:52 +0530

    bug fixed

--- src/app/features/employee/employee.module.ts
+++ src/app/features/employee/employee.module.ts
@@ -1,16 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClarityModule } from '@clr/angular';
+import { SharedModule } from '../../shared/shared.module';
 import { EmployeeChartComponent } from './components/employee-chart/employee-chart.component';
-import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
 import { EmployeeGridComponent } from './components/employee-grid/employee-grid.component';
 import { AddEmployeeComponent } from './dialogs/add-employee/add-employee.component';
 import { DeleteEmployeeComponent } from './dialogs/delete-employee/delete-employee.component';
-import { UpdateEmployeeComponent } from './dialogs/update-employee/update-employee.component';
-import { SharedModule } from '../../shared/shared.module';
-import { FormsModule } from '@angular/forms';
 import { FormEmployeeComponent } from './dialogs/form-employee/form-employee.component';
+import { UpdateEmployeeComponent } from './dialogs/update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +18,6 @@ import { FormEmployeeComponent } from './dialogs/form-employee/form-employee.com
     DeleteEmployeeComponent,
     UpdateEmployeeComponent,
     AddEmployeeComponent,
-    EmployeeFormComponent,
     FormEmployeeComponent,
   ],
   imports: [
